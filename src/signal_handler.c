2fd7eaea6caa3d95bf2827a39373b8923d4f2d5c
George Fotopoulos <xorz57@gmail.com>
2023-03-12 03:59:43 +0200

    Update signal_handler.c

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -10,11 +10,7 @@ extern volatile sig_atomic_t signal_status;
 
 void signal_handler(int signal) {
     signal_status = signal;
-    switch (signal_status) {
-        case SIGINT:
-            exit(EXIT_FAILURE);
-            break;
-        default:
-            break;
+    if (signal_status == SIGINT) {
+        exit(EXIT_FAILURE);
     }
 }
